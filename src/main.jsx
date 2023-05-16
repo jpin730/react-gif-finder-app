3c7e9e152db6c737363b6190231f70fb58d6019c
Jaime Pineda <jpin730@pm.me>
2023-05-15 18:08:05 -0600

    chore: :wrench: set up .env file

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,17 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <GifFinderApp />
   </React.StrictMode>
 );
+
+// Testing .env file
+fetch(
+  `https://api.giphy.com/v1/gifs/random?api_key=${
+    import.meta.env.VITE_GIPHY_API_KEY
+  }`
+)
+  .then((res) => res.json())
+  .then(({ data }) => {
+    const img = document.createElement("img");
+    const { url } = data.images.original;
+    img.src = url;
+    document.body.appendChild(img);
+  });
