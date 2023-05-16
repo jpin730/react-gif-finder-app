17414610e016d7d56330a365351ba5ad6de55e09
Jaime Pineda <jpin730@pm.me>
2023-05-16 13:57:42 -0600

    style: :lipstick: add styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GifFinderApp } from "./GifFinderApp";
 
+import "./styles.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GifFinderApp />
