e2d963ce28ccb2217ae891a4a0a5d316cbbcdf7b
Jaime Pineda <jpin730@pm.me>
2023-05-17 13:36:11 -0600

    test: :white_check_mark: add gif item tests

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,8 +1,15 @@
+import PropTypes from "prop-types";
+
 export const GifItem = ({ title, url }) => {
   return (
     <div className="card" title={title}>
       <img src={url} alt={title} />
-      <p>{title}</p>
+      <p data-testid="p-title">{title}</p>
     </div>
   );
+};
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components";
+
+describe("GifItem", () => {
+  const title = "mocked-title";
+  const url = "http://mocked-url/";
+
+  test("should match snapshot", () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should set correct url and alt", () => {
+    render(<GifItem title={title} url={url} />);
+
+    const { src, alt } = screen.getByRole("img");
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("should show title as a paragraph", () => {
+    render(<GifItem title={title} url={url} />);
+
+    const { innerHTML } = screen.getByTestId("p-title");
+
+    expect(innerHTML).toContain(title);
+  });
+});
