85578cb333a5d48581e167176c5c3ea12b4d7a33
Jaime Pineda <jpin730@pm.me>
2023-05-17 13:07:52 -0600

    refactor: :recycle: refactor enviromment variables for testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,7 @@
 import "whatwg-fetch";
+
+jest.mock("./src/constants", () => ({
+  ENV: {
+    VITE_GIPHY_API_KEY: "MockedKey",
+  },
+}));
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,8 @@
+import { ENV } from "../constants";
+
 export const getGifs = async (category) => {
   const apiUrl = "https://api.giphy.com/v1/gifs/search";
-  const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
+  const apiKey = ENV.VITE_GIPHY_API_KEY;
   const limit = 12;
   const url = `${apiUrl}?api_key=${apiKey}&q=${category}&limit=${limit}`;
 
